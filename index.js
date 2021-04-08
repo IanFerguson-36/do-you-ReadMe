4775eb5d8bb6f1ab34e817343e378c9b75a29d3e
Ian F <ian.ferg36@gmail.com>
2021-04-08 14:36:51 -0600

    began writefile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const rs = require("fs");
+const fs = require("fs");
+const { fstat } = require("node:fs");
 // TODO: Create an array of questions for user input
 const questions = [
   inquirer.prompt([
@@ -28,7 +29,11 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile("customReadme.md", process.argv[2], (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
