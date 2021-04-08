ff99eef43b3967a49d40981e6cc0f0ccd073ad57
Ian F <ian.ferg36@gmail.com>
2021-04-08 14:10:39 -0600

    initial template for user questions

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const rs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your title?",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "What is its description?",
+      name: "Description",
+    },
+    {
+      type: "input",
+      message: "What is the installation process?",
+      name: "Install",
+    },
+    {
+      type: "input",
+      message: "License?",
+      name: "License",
+    },
+  ]),
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
