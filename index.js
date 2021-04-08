f1cf2551bc37fd2c3fae53502c1a2b025b39598c
Ian F <ian.ferg36@gmail.com>
2021-04-08 15:40:45 -0600

    created inquirer prompt

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { fstat } = require("node:fs");
+
 // TODO: Create an array of questions for user input
 const questions = [
-  inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your title?",
-      title: "Title",
-    },
-    {
-      type: "input",
-      message: "What is its description?",
-      name: "Description",
-    },
-    {
-      type: "input",
-      message: "What is the installation process?",
-      name: "Install",
-    },
-    {
-      type: "input",
-      message: "License?",
-      name: "License",
-    },
-  ]),
+  {
+    type: "input",
+    message: "What is your title?",
+    name: "Title",
+  },
+  {
+    type: "input",
+    message: "What is its description?",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "What is the installation process?",
+    name: "Install",
+  },
+  {
+    type: "input",
+    message: "License?",
+    name: "License",
+  },
 ];
-
+inquirer.prompt(questions).then((answers) => {
+  const readMe = `#Title ${questions[0].name}
+  #Description ${questions[1].name}
+  #Installation Process ${questions[2].name}
+  #License ${questions[3].name}
+    
+    `;
+  console.log(readMe);
+});
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  const readMe = `#Title
-    ${questions.title}
-    `;
-
   fs.writeFile("customReadme.md", process.argv[2], (err) =>
     err ? console.error(err) : console.log("Success!")
   );
