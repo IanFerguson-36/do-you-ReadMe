ff13e33190c1d0377b8bd050a0b5f339933c8cb0
Ian F <ian.ferg36@gmail.com>
2021-04-08 16:38:19 -0600

    connected with generatemarkdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
   {
@@ -25,24 +25,26 @@ const questions = [
     name: "License",
   },
 ];
-inquirer.prompt(questions).then((answers) => {
-  const readMe = `#Title ${questions[0].name}
-  #Description ${questions[1].name}
-  #Installation Process ${questions[2].name}
-  #License ${questions[3].name}
-    
-    `;
-  console.log(readMe);
-});
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  fs.writeFile("customReadme.md", process.argv[2], (err) =>
-    err ? console.error(err) : console.log("Success!")
-  );
-}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+    // .then((answers) => {
+    //   const readMe = `#Title ${questions[0].name}
+    // #Description ${questions[1].name}
+    // #Installation Process ${questions[2].name}
+    // #License ${questions[3].name}`;
+    //   console.log(answers);
+    // })
+
+    .then((answers) => {
+      const htmlPageContent = generateMarkdown(answers);
+      fs.writeFile("customReadMe.md", htmlPageContent, (err) =>
+        err ? console.log(err) : console.log("Successfully created ReadMe!")
+      );
+    });
+}
 
 // Function call to initialize app
 init();
